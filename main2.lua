ac2ccf84f6070f0cee7423c4ac557ad4610249ca
JualNasiRendang <radikaradikaff@gmail.com>
2026-09-09 13:13:29 +0700

    Create main2.lua

---
+++ main2.lua
@@ -0,0 +1,2 @@
+getgenv().SCRIPT_KEY = "KEYLESS"
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/c090866e4201baecfe09dce16546ea747661d2d0861ae16f1eacf1d98dee3e3c/download"))()
