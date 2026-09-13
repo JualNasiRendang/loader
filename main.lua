27a5d96c59faff9d0dfb861851ac3bad7895faf0
JualNasiRendang <radikaradikaff@gmail.com>
2026-09-14 02:10:08 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 -- This file was protected using Luraph Obfuscator v15.0 [https://lura.ph/]
-loadstring(game:HttpGet("https://www.nrlscript.com/raw/wrijQVqSGC"))()
+-- loadstring(game:HttpGet("https://www.nrlscript.com/raw/wrijQVqSGC"))()
 
 -- This file was protected using
 
